8bfc6012adea7e62fa50c2fbd67d695e65c9e167
张松超 <zhangsongchao@ucfgroup.com>
2015-07-27 17:52:33 +0800

    创建一个test proj

---
+++ cocoaPodTest/cocoaPodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoaPodTest
+//
+//  Created by zsc on 15/7/27.
+//  Copyright (c) 2015年 UCF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoaPodTest/cocoaPodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoaPodTest
+//
+//  Created by zsc on 15/7/27.
+//  Copyright (c) 2015年 UCF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
